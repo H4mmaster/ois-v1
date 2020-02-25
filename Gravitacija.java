03eccadb7f9f831999fd552b912c657e96b4e708
H4mmaster <jakobdittmayer@gmail.com>
2020-02-25 21:09:18 +0000

    Gravitacija.java

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Gravitacija {
     public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
         System.out.println("OIS can go fuck itself");
     }
 }
