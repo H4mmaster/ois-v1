3fd1f757048dbf4b1456e1a5ef6bb5dc189b59b6
tilenl <tilenlampret1@gmail.com>
2020-02-27 18:13:19 +0000

    Zdej upam da je taprav tilenl

--- Gravitacija.java
+++ Gravitacija.java
@@ -4,7 +4,7 @@ public class Gravitacija {
     public static void main(String args[]) {
     }
     
-    //spremenljivke potrebne za izracun gravitacijskega pospeska
+    //spremenljivke potrebne za izracun gravitacijskega pospeska!
     private static double C = 6.674e-11;
     private static double M = 5.972e24;
     private static double r = 6.371e6;
