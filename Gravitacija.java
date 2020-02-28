b768f0d236364f04811055328dfd82849d0655a5
H4mmaster <jakobdittmayer@gmail.com>
2020-02-28 09:35:58 +0000

    Zdej uporablja args

--- Gravitacija.java
+++ Gravitacija.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Gravitacija {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        int visina = Integer.parseInt(args[0]);
         
-        System.out.println("Muricas fuck ye");
+        System.out.println(visina);
     }
 }
